c929364883df8cd85957049d746d6385b5aab5d6
Maxim Zimaliev <zimaliev@yandex.ru>
2015-04-17 15:01:14 +0300

    firefox decode slash

--- src/Routing.purs
+++ src/Routing.purs
@@ -25,10 +25,13 @@ foreign import decodeURIComponent :: String -> String
 foreign import hashChanged """
 function hashChanged(handler) {
   return function() {
-    var currentHash = document.location.hash;
-    handler("")(currentHash)();
+    var getHash = function() {return document.location.href.split('#')[1] || '';};
+    window['__oldHash'] = "";
+    handler("")(getHash())();
     window.addEventListener("hashchange", function(ev) {
-      handler(ev.oldURL)(ev.newURL)();
+      var newHash = getHash();
+      handler(window['__oldHash'])(newHash)();
+      window['__oldHash'] = newHash;
     });
   };
 }
