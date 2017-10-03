de41517dcbe69c3975c1b59b575d3972a4ff9984
Marcin Szamotulski <mszamot@gmail.com>
2017-10-03 22:34:59 +0200

    Ord instance for RoutePart

--- src/Routing/Types.purs
+++ src/Routing/Types.purs
@@ -2,10 +2,12 @@ module Routing.Types where
 
 import Data.List (List)
 import Data.Map (Map)
+import Data.Ord (class Ord)
 import Prelude (class Eq)
 
 data RoutePart = Path String | Query (Map String String)
 
 derive instance eqRoutePart :: Eq RoutePart
+derive instance ordRoutePart :: Ord RoutePart
 
 type Route = List RoutePart
