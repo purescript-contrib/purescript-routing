2ee0c88a1af6b8c42fa2d00af1ab0281e919b47d
Marcin Szamotulski <mszamot@gmail.com>
2017-10-03 21:59:18 +0200

    Eq instance for RoutePart

--- src/Routing/Types.purs
+++ src/Routing/Types.purs
@@ -1,7 +1,11 @@
 module Routing.Types where
 
-import Data.List (List())
-import Data.Map (Map())
+import Data.List (List)
+import Data.Map (Map)
+import Prelude (class Eq)
 
 data RoutePart = Path String | Query (Map String String)
+
+derive instance eqRoutePart :: Eq RoutePart
+
 type Route = List RoutePart
