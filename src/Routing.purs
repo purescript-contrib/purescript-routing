25358da1b9bde001285a0c1ffd89fa5dce3c3b92
Miles Frain <miles.frain@colorado.edu>
2020-12-16 14:40:15 -0800

    Remove helper function

--- src/Routing.purs
+++ src/Routing.purs
@@ -12,13 +12,9 @@ import Partial.Unsafe(unsafePartial)
 import Routing.Match (Match, runMatch)
 import Routing.Parser (parse)
 
-unsafeDecodeURIComponent :: String -> String
-unsafeDecodeURIComponent uri =
-  unsafePartial $ fromJust $ decodeURIComponent uri
-
 -- | Runs a `Match` parser.
 match :: forall a. Match a -> String -> Either String a
-match = matchWith unsafeDecodeURIComponent
+match = matchWith $ unsafePartial fromJust <<< decodeURIComponent
 
 -- | Runs a `Match` parser given a custom String decoder.
 matchWith :: forall a. (String -> String) -> Match a -> String -> Either String a
